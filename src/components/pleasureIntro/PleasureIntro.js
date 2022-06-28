c506c0fd7ed9f850e6d6a69d824473d0239a0cfd
DmitryBavelko-98 <bavelko90@gmail.com>
2022-06-28 14:40:38 +0300

    Add pleasure page intro

---
+++ src/components/pleasureIntro/PleasureIntro.js
@@ -0,0 +1,11 @@
+import './pleasureIntro.scss'
+
+const PleasureIntro = () => {
+    return (
+        <section className="pleasure-intro">
+            <h1 className="pleasure-intro__title main-title">For your pleasure</h1>
+        </section>
+    );
+}
+
+export default PleasureIntro;
